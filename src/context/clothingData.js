10b6cd3f35cbf8dbd928fac185b65ed11e5e3474
DenooThomas <denoo.thomas@gmail.com>
2022-11-29 13:32:48 +0100

    changed clothingData

--- src/context/clothingData.js
+++ src/context/clothingData.js
@@ -11,15 +11,15 @@ const clothingData = [
         id: "2",
         category: "upperbody",
         title: "Tshirt",
-        description: "Tshirt suprememe",
+        description: "Black t-shirt",
         price: "15.50",
         image: "https://cdn.shopify.com/s/files/1/0309/1926/2267/products/S.04.CST.0121_220_01_02f0bd12-87b9-4be5-81a2-425d68884b21_1200x.png"
     },
     {
         id: "3",
         category: "lowerbody",
-        title: "pants long",
-        description: "Longgg pants",
+        title: "Blue trousers",
+        description: "Circular Wool Flannel by Vitale Barberis Canonico, Italy",
         price: "49.99",
         image: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fa3%2F58%2Fa358537a5fd396056bec1b370ecb0b371887c555.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_trousers_trousers_skinny_all%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
 
@@ -27,32 +27,32 @@ const clothingData = [
     {
         id: "4",
         category: "lowerbody",
-        title: "pants short",
-        description: "Shorttt pants",
+        title: "Shorts - grey",
+        description: "Camel active",
         price: "39.99",
         image: "https://cdn.shopify.com/s/files/1/0085/5012/1535/products/mr_marvis_pique_Felsons_PCK_F1_2x3_Grey_500x750.jpg"
     },
     {
         id: "5",
         category: "shoes",
-        title: "Herenschoenen",
-        description: "Dit paar camelkleurige leren kostuumschoenen, met reliëfdetails op de achterkant, is ideaal voor onder je kostuum en geeft je outfit een elegante noot.",
+        title: "Brown Oxfords",
+        description: "These timeless brown Oxfords are crafted from supple calf leather in a flexible Blake stitch, and feature full leather lining and sole.",
         price: "129.99",
         image: "https://www.torfs.be/dw/image/v2/BCQR_PRD/on/demandware.static/-/Sites-torfs-catalog-master/default/dwe0930424/hi-res/pdp/311/311016/311016-4.jpg?sw=250"
     },
     {
         id: "6",
         category: "shoes",
-        title: "Herenschoenen (marine blauw)",
-        description: "Kleur eens buiten de lijntjes om een klassiek kostuum te verfraaien, met dit paar glad leren schoenen.",
+        title: "Blue Brogue Double Lace-up Strap",
+        description: "Made from supple calf leather, these refined blue lace-ups feature full leather lining and sole. They are craft in a Blake stitch with a brogue design.",
         price: "165.00",
         image: "https://www.torfs.be/dw/image/v2/BCQR_PRD/on/demandware.static/-/Sites-torfs-catalog-master/default/dwe46b29c5/hi-res/pdp/309/309585/309585-4.jpg?sw=250"
     },
     {
         id: "7",
         category: "upperbody",
-        title: "Men Plaid Single Button Blazer",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed porttitor velit, non volutpat arcu. Morbi sollicitudin.",
+        title: "Dark Grey Havana Blazer",
+        description: "Available on its own or as a full suit, this handsome dark grey Havana blazer is tailored to a slim fit with peak lapels, a natural shoulder and 2.5-button front—all in the soft hand of a brushed flannel finish.",
         price: "19.00",
         image: "https://img.ltwebstatic.com/images3_pi/2021/12/27/164059160513d2fe32448774868a01bad31af4c6af_thumbnail_900x.webp"
     },
